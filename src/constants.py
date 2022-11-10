c5ad2893b843369b76ff8a6b9fc51232f7086d30
Kali Love <kali.love@klovetech.com>
2022-11-11 00:48:27 +0300

    changed jupyter notebook

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,4 @@
-TAGS = [""]
+
+
+# this file will store all of the constants/variables we will use
+TAGS = ["knowledge", "wisdom", "life-lessons", "life"]
---
+++ src/scraper.py
@@ -0,0 +1,25 @@
+import requests as _request
+import bs4 as _bs4
+
+
+# define function to format the url
+def _create_url(tag: str) -> str:
+    return f"https://www.goodreads.com/quotes/tag/{tag}"
+
+
+# define function to get page and make it an object
+def _get_page(url: str) -> _bs4.BeautifulSoup:
+    # request the url using the "get" method
+    page = _request.get(url)
+    # create the BeautifulSoup object
+    soup = _bs4.BeautifulSoup(page.content, "html.parser")
+    # return the BeautifulSoup object
+    return soup
+
+
+# create url function
+url = _create_url("knowledge")
+# create soup function
+soup = _get_page(url)
+
+print(soup.find_all(class_="quoteText"))
